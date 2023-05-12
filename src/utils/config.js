dfcf62c03db97c55450474c7673dcbf23e3b9e18
ManiTej31 <manitejaaithu@gmail.com>
2023-05-12 11:09:19 +0530

    first commit

---
+++ src/utils/config.js
@@ -0,0 +1,18 @@
+//TODO: add firebase configuration and export it
+export const firebaseConfig = {
+  //TODO: ADD YOUR FIREBASE INFO HERE
+  apiKey: "AIzaSyCp4z5XtRDh4w7Bkh7qClZeAiG6kUKvG8U",
+  authDomain: "mygitapp-6dc54.firebaseapp.com",
+  projectId: "mygitapp-6dc54",
+  storageBucket: "mygitapp-6dc54.appspot.com",
+  messagingSenderId: "205132979111",
+  appId: "1:205132979111:web:8f75176bebba2765287482",
+  measurementId: "G-N9YYQWCTG4"
+};
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true
+};
